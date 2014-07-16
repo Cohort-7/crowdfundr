100ff16fc928d2ce97bd866c3cb00a54521b83e8
brandoshmando <brancraft@gmail.com>
2014-07-16 14:54:19 -0400

    Added some stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to
+    else
+      flash.now[:alert] = "New user was not saved..."
+      render :new
+    end
   end
 
   def show
@@ -20,5 +28,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
