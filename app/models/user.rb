f76ded5d7e376fb1d3d31d6a202764719db2eda7
Vladimir Dralo <svolard@gmail.com>
2014-07-17 01:58:27 -0400

    Added full_name method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: {minimum: 6}, on: :create
 
+  def full_name
+  	"#{first_name} #{last_name}"
+  end
+
 end
