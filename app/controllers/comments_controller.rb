11bde8e057c795e4d93a4cfb7b1c7c933be483a6
Vladimir Dralo <svolard@gmail.com>
2014-07-18 15:14:58 -0400

    Comments WiP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
 	def create
 		@project = Project.find(params[:project_id])
 		@comment = @project.comments.new(text: params[:comment][:text])
+		@comment.user = current_user
 		if @comment.save
 			redirect_to @project
 		else
