b61c8f47ead297614df4207f791204c6aa30f236
brandoshmando <brancraft@gmail.com>
2014-07-19 14:08:23 -0400

    Added search div and partial with necessary tags to index page

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$("#category").on('change', function() {
 		location.href = $(this).val()
 	});
+
+  $('#search').on('submit', function(ev){
+    ev.preventDefatult();
+  });
 });
