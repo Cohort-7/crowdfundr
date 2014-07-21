d59661564d3dd949aee786be4ef4e6f3e6d608bd
brandoshmando <brancraft@gmail.com>
2014-07-20 20:05:33 -0400

    Logo is now link to home

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   #Create and return a gavatarified url for an image tag
   def gravatarify_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
+    "http://gravatar.com/avatar/#{gravatar_id}?s=200"
   end
 
   def owner?(user)
